a786143ed92204405919062d302c8514b076e763
Vanfarock <vinicius.m.m.2002@gmail.com>
2021-08-16 22:04:21 -0300

    Programa base dockerizado

---
+++ download-dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Baixa o git
+apk add git
+
+# Baixa o gin
+go get -u github.com/gin-gonic/gin
--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Bla struct {
+	X int
+}
+
 func main() {
 	router := gin.Default()
 
@@ -13,28 +17,3 @@ func main() {
 
 	router.Run()
 }
-
-// func main() {
-// 	router := gin.Default()
-
-// 	router.GET("/", func(c *gin.Context) {
-// 		c.String(http.StatusOK, "Hello")
-// 	})
-
-// 	router.GET("/:name", func(c *gin.Context) {
-// 		nome := c.Param("name")
-// 		mensagem := fmt.Sprintf("Ola %s", nome)
-// 		c.String(http.StatusOK, mensagem)
-// 	})
-
-// 	router.POST("/heroi", func(c *gin.Context) {
-// 		var input models.SuperHeroi
-// 		if err := c.ShouldBindJSON(&input); err != nil {
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		}
-
-// 		c.String(http.StatusOK, fmt.Sprintf("Super heroi %s criado", input.Nome))
-// 	})
-
-// 	router.Run()
-// }
